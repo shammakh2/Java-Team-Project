a14f48edaf52ac55eca874e55e12e7c1b4d4c7a0
b9029625 <t.england2@newcastle.ac.uk>
2020-02-26 11:13:11 +0000

    Started method for getting data to output

--- src/main/java/csc1035/project3/HibernateUtil.java
+++ src/main/java/csc1035/project3/HibernateUtil.java
@@ -26,4 +26,3 @@ public class HibernateUtil {
         getSessionFactory().close();
     }
 }
-
--- src/main/java/csc1035/project3/lookup/stockLookup.java
+++ src/main/java/csc1035/project3/lookup/stockLookup.java
@@ -2,6 +2,7 @@ package csc1035.project3.lookup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class stockLookup {
     public static void main(String[] args){
@@ -10,6 +11,20 @@ public class stockLookup {
         List<Item> test_list = new ArrayList<>();
         test_list.add(test_item);
         output(test_list);
+        get_data_id();
+    }
+
+    public static void get_data_id(){
+        List<Item> data = new ArrayList<>();
+
+        Scanner id_scanner = new Scanner(System.in);
+        System.out.print("Enter item ID \n>>>  ");
+        String searchItem = id_scanner.nextLine();
+        System.out.println("Searching for : " + searchItem);
+
+        // Code here to search DB once its made
+
+        // Code here to run output with the list
     }
 
     public static void output(List<Item> data){
