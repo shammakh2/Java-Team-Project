2d382723ab7cb6f69d49b691205e72207506c61f
b9029625 <t.england2@newcastle.ac.uk>
2020-02-27 14:05:14 +0000

    Updated lookup to work with database

--- src/main/java/csc1035/project3/lookup/stockLookup.java
+++ src/main/java/csc1035/project3/lookup/stockLookup.java
@@ -1,10 +1,12 @@
 package csc1035.project3.lookup;
 
 import csc1035.project3.HibernateUtil;
+import csc1035.project3.insert.Table_Initializer;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,11 +18,12 @@ public class stockLookup {
         test_list.add(test_item);
         test_list.add(test_item);
         output(test_list);*/
-        get_data_id();
+        //get_data_id();
+        get_data_name();
     }
 
     public static void get_data_name(){
-        List<Item> data = new ArrayList<>();
+        List<Table_Initializer> data = new ArrayList<>();
 
         Scanner id_scanner = new Scanner(System.in);
         System.out.print("Enter item name \n>>>  ");
@@ -29,17 +32,40 @@ public class stockLookup {
 
         // Code here to search DB once its made
 
+        //Read
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            List stock = session.createQuery("FROM Stock").list();
+            for (Iterator<Table_Initializer> iterator = stock.iterator(); iterator.hasNext();){
+                Table_Initializer item = iterator.next();
+                if(item.getName() instanceof String) {
+                    if (item.getName().equals(search_item_name)) {
+                        data.add(item);
+                    }
+                } else {
+                    System.out.println("Warning: item ID " + item.getId() + " has no name");
+                }
+            }
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (session!=null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
         // Code here to run output with the list
         output(data);
     }
 
 
     public static void get_data_id(){
-        List<Item> data = new ArrayList<>();
+        List<Table_Initializer> data = new ArrayList<>();
 
         Scanner id_scanner = new Scanner(System.in);
         System.out.print("Enter item ID \n>>>  ");
-        String search_item_id = id_scanner.nextLine();
+        int search_item_id = Integer.valueOf(id_scanner.nextLine());
         System.out.println("Searching for ID: " + search_item_id);
 
         // Code here to search DB once its made
@@ -48,10 +74,10 @@ public class stockLookup {
         Session session = HibernateUtil.getSessionFactory().openSession();
         try {
             session.beginTransaction();
-            List cars = session.createQuery("FROM CAR").list();
-            for (Iterator<Item> iterator = cars.iterator(); iterator.hasNext();){
-                Item item = iterator.next();
-                if(item.getID() == search_item_id){
+            List stock = session.createQuery("FROM Stock").list();
+            for (Iterator<Table_Initializer> iterator = stock.iterator(); iterator.hasNext();){
+                Table_Initializer item = iterator.next();
+                if(item.getId() == search_item_id){
                     data.add(item);
                 }
             }
@@ -67,7 +93,7 @@ public class stockLookup {
         output(data);
     }
 
-    public static void output(List<Item> data){
+    public static void output(List<Table_Initializer> data){
 
         // Function outputs a table containing the results of the database query.
 
@@ -76,8 +102,8 @@ public class stockLookup {
         System.out.format("+-----------------+------------+------------+%n");
         System.out.format("| Item ID         | Item Name  |  Quantity  |%n");
         System.out.format("+-----------------+------------+------------+%n");
-        for (Item item : data) {
-            System.out.format(align, item.id, item.name, item.quantity);  // Prints out database row
+        for (Table_Initializer item : data) {
+            System.out.format(align, item.getId(), item.getName(), item.getStock());  // Prints out database row
         }
         System.out.format("+-----------------+------------+------------+%n");
     }
