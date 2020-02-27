f60ace8ec0afac3269dbfaceb8a4ee5788555439
Nicholas Ingram <b9018614@newcastle.ac.uk>
2020-02-27 13:37:56 +0000

    Created PrintReceipt class as template for printing a console style receipt and defined a ShoppingList (similar to Entry within last project) in order to move shopping basket values to receipt

---
+++ src/main/java/csc1035/project3/ShoppingList.java
@@ -0,0 +1,23 @@
+package csc1035.project3;
+
+public class ShoppingList {
+    int productID;
+    int quantity;
+    float price;
+
+    public ShoppingList(int productID, int quantity, float price){
+        this.productID = productID;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getProductID(){
+        return productID;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public float getPrice(){
+        return price;
+    }
+}
