0591cd7d5df9c550d9888dcf4325c8539a8e3621
b9029625 <t.england2@newcastle.ac.uk>
2020-02-28 14:52:03 +0000

    Added sell price to lookup output

--- src/main/java/csc1035/project3/lookup/stockLookup.java
+++ src/main/java/csc1035/project3/lookup/stockLookup.java
@@ -175,18 +175,19 @@ public class stockLookup {
             }
         }
 
-        String align = "| %-15s | %-"+max_length+"s | %-10s | %-10s | %-10s |%n";  // Formatting key for table
+        String align = "| %-15s | %-"+max_length+"s | %-10s | %-10s | %-10s | %-10s |%n";  // Formatting key for table
 
         String row_text = String.format(String.format("%%%ds", max_length + 2), " ").replace(" ", "-");
         String title_text = String.format(String.format("%%%ds", max_length - 9), " ");
 
-        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+%n");
-        System.out.format("| Item ID         | Item Name"+title_text+" |  Quantity  | Category   | Cost       |%n");
-        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+%n");
+        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+------------+%n");
+        System.out.format("| Item ID         | Item Name"+title_text+" |  Quantity  | Category   | Cost       | Sell Price |%n");
+        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+------------+%n");
         for (Table_Initializer item : data) {
-            System.out.format(align, item.getId(), item.getName(), item.getStock(), item.getCategory(), item.getCost());  // Prints out database row
+            System.out.format(align, item.getId(), item.getName(), item.getStock(), item.getCategory(), item.getCost(), item.getSell_price());
+            // Prints out database row
         }
-        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+%n");
+        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+------------+%n");
     }
 
 }
