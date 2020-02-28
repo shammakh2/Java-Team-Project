5c66254c5f0bbd30aa82a747d8f39b5ab02480e4
b9029625 <t.england2@newcastle.ac.uk>
2020-02-28 13:58:43 +0000

    Made a basic ui

---
+++ src/main/java/csc1035/project3/ui/ui.java
@@ -0,0 +1,48 @@
+package csc1035.project3.ui;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ui {
+    public static ArrayList<String> options = new ArrayList<>(Arrays.asList("lookup", "exit"));
+    public static void main(String[] args){
+        boolean running = true;
+        System.out.println("Welcome");
+        while(running) {
+            System.out.println("What would you like to do?");
+            Scanner scanner = new Scanner(System.in);
+            boolean valid = false;
+            String choice = "";
+            while (!valid) {
+                System.out.print(">>>  ");
+                choice = scanner.nextLine().toLowerCase();
+                if (options.contains(choice)) {
+                    valid = true;
+                } else if (choice.equals("help")) {
+                    help();
+                } else {
+                    System.out.println("Invalid Choice, please try again");
+                }
+            }
+            /*
+            * Please add your functions below.
+            */
+            String[] arguments = new String[]{"123"};  // use if running a main method.
+            if (choice.equals("lookup")) {
+                csc1035.project3.lookup.stockLookup.main(arguments);
+            }
+            if (choice.equals("exit")) {
+                running = false;
+                System.out.println("Exiting program, goodbye.");
+            }
+        }
+
+    }
+
+    public static void help(){
+        for(String option : options){
+            System.out.println(option);
+        }
+    }
+}
