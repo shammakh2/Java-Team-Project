a87c4c4a45d46789f270ebf4d924fc5821ce9da3
b9029625 <t.england2@newcastle.ac.uk>
2020-02-28 15:00:40 +0000

    Changed multiple functions for lookup into one

--- src/main/java/csc1035/project3/lookup/stockLookup.java
+++ src/main/java/csc1035/project3/lookup/stockLookup.java
@@ -18,31 +18,35 @@ public class stockLookup {
         System.out.print("Enter search type\n>>>  ");
         String choice = type_scanner.nextLine();
         if(choice.toLowerCase().equals("id")){
-            get_data_id();
+            //get_data_id();
+            get_data("id");
         } else if (choice.toLowerCase().equals("name")){
-            get_data_name();
+            //get_data_name();
+            get_data("name");
         } else if (choice.toLowerCase().equals("category")){
-            get_data_category();
+            //get_data_category();
+            get_data("category");
+        } else {
+            System.out.println("Invalid category");
         }
     }
-
     /**
-     * Searches the Stock table for item names that contain or match a search term.
+     * Searches the Stock table for items.
      */
-    public static void get_data_name(){
+    public static void get_data(String type){
         /*
           List containing results to be outputted later.
          */
         List<Table_Initializer> data = new ArrayList<>();
 
-        Scanner name_scanner = new Scanner(System.in);
-        System.out.print("Enter item name \n>>>  ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter item "+type+" \n>>>  ");
         /*
           User entered search term
          */
-        String search_item_name = name_scanner.nextLine();
-        System.out.println("Searching for Item: " + search_item_name);
-        search_item_name = search_item_name.toLowerCase();
+        String search_item = scanner.nextLine();
+        System.out.println("Searching for "+type+": " + search_item);
+        search_item = search_item.toLowerCase();
 
         // Read
         Session session = HibernateUtil.getSessionFactory().openSession();
@@ -51,65 +55,35 @@ public class stockLookup {
             List stock = session.createQuery("FROM Stock").list();
             for (Iterator<Table_Initializer> iterator = stock.iterator(); iterator.hasNext();){
                 Table_Initializer item = iterator.next();
-                if(item.getName() != null) {
-                    if (item.getName().toLowerCase().equals(search_item_name) || item.getName().toLowerCase().
-                            contains(search_item_name)) {
+                if(type.equals("name")) {
+                    if (item.getName() != null) {
+                        if (item.getName().toLowerCase().equals(search_item) || item.getName().toLowerCase().
+                                contains(search_item)) {
+                            data.add(item);
+                        }
+                    } else {
+                        /*
+                          Warning for if item name is null.
+                         */
+                        System.out.println("Warning: item ID " + item.getId() + " has no name");
+                    }
+                } else if (type.equals("id")){
+                    int search_item_id = Integer.valueOf(search_item);
+                    if(item.getId() == search_item_id){
                         data.add(item);
                     }
-                } else {
+                } else if (type.equals("category")){
+                    if(item.getName() != null) {
+                        if (item.getCategory().toLowerCase().equals(search_item) || item.getCategory().toLowerCase().
+                                contains(search_item)) {
+                            data.add(item);
+                        }
+                    } else {
                     /*
                       Warning for if item name is null.
                      */
-                    System.out.println("Warning: item ID " + item.getId() + " has no name");
-                }
-            }
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session!=null) session.getTransaction().rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        // Output
-        output(data);
-    }
-
-    /**
-     * Searches the Stock table for item categories that contain or match a search term.
-     */
-    public static void get_data_category(){
-        /*
-          List containing results to be outputted later.
-         */
-        List<Table_Initializer> data = new ArrayList<>();
-
-        Scanner cat_scanner = new Scanner(System.in);
-        System.out.print("Enter category \n>>>  ");
-        /*
-          User entered search term
-         */
-        String search_item_cat = cat_scanner.nextLine();
-        System.out.println("Searching for Item: " + search_item_cat);
-        search_item_cat = search_item_cat.toLowerCase();
-
-        // Read
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            session.beginTransaction();
-            List stock = session.createQuery("FROM Stock").list();
-            for (Iterator<Table_Initializer> iterator = stock.iterator(); iterator.hasNext();){
-                Table_Initializer item = iterator.next();
-                if(item.getName() != null) {
-                    if (item.getCategory().toLowerCase().equals(search_item_cat) || item.getCategory().toLowerCase().
-                            contains(search_item_cat)) {
-                        data.add(item);
+                        System.out.println("Warning: item ID " + item.getId() + " has no category");
                     }
-                } else {
-                    /*
-                      Warning for if item name is null.
-                     */
-                    System.out.println("Warning: item ID " + item.getId() + " has no category");
                 }
             }
             session.getTransaction().commit();
@@ -124,45 +98,6 @@ public class stockLookup {
         output(data);
     }
 
-    /**
-     * Searches the Stock table for item ids that match a search term.
-     */
-    public static void get_data_id(){
-        /*
-          List containing results to be outputted later.
-         */
-        List<Table_Initializer> data = new ArrayList<>();
-
-        Scanner id_scanner = new Scanner(System.in);
-        System.out.print("Enter item ID \n>>>  ");
-        /*
-          User entered search term
-         */
-        int search_item_id = Integer.valueOf(id_scanner.nextLine());
-        System.out.println("Searching for ID: " + search_item_id);
-
-        // Read
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            session.beginTransaction();
-            List stock = session.createQuery("FROM Stock").list();
-            for (Iterator<Table_Initializer> iterator = stock.iterator(); iterator.hasNext();){
-                Table_Initializer item = iterator.next();
-                if(item.getId() == search_item_id){
-                    data.add(item);
-                }
-            }
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session!=null) session.getTransaction().rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        // Output
-        output(data);
-    }
     /**
      * Outputs the provided data in an ascii table
      */
