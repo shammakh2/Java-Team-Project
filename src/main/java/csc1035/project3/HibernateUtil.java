206c4d102b81398ea3b87359878b3c0ba5008422
b9003599 <b9003599@newcastle.ac.uk>
2020-03-03 05:59:16 +0000

    Start

--- src/main/java/csc1035/project3/HibernateUtil.java
+++ src/main/java/csc1035/project3/HibernateUtil.java
@@ -10,7 +10,6 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
 
         try {
-
             return new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex){
             System.err.println("Initial SessionFactory creation failed." + ex);
