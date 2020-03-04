b6eb2050955b0550b02f691daa1ceb0c56e3e6e4
eliotth <b9055504@newcastle.ac.uk>
2020-03-04 11:03:47 +0000

    deleted the output method from the warning_quantity class

--- src/main/java/csc1035/project3/warning/warning_quantity.java
+++ src/main/java/csc1035/project3/warning/warning_quantity.java
@@ -33,28 +33,4 @@ public class warning_quantity {
         output.output(data);
         System.out.println("The quantity of these products are low, you should order some more.");
     }
- /*   private static void output(List<Table_Initializer> data){
-
-        int max_length = 10;
-        for(Table_Initializer item: data){
-            if(item.getName().length() > max_length){
-                max_length = item.getName().length();
-            }
-        }
-
-        String align = "| %-15s | %-"+max_length+"s | %-10s | %-10s | %-10s | %-10s | %-10s |%n";  // Formatting key for table
-
-        String row_text = String.format(String.format("%%%ds", max_length + 2), " ").replace(" ", "-");
-        String title_text = String.format(String.format("%%%ds", max_length - 9), " ");
-
-        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+------------+------------+%n");
-        System.out.format("| Item ID         | Item Name"+title_text+" |  Quantity  | Category   | Cost       | Sell Price | Perishable |%n");
-        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+------------+------------+%n");
-        for (Table_Initializer item : data) {
-            System.out.format(align, item.getId(), item.getName(), item.getStock(), item.getCategory(), item.getCost(), item.getSell_price(), item.getPerishable());
-            // Prints out database row
-        }
-        System.out.format("+-----------------+"+row_text+"+------------+------------+------------+------------+------------+%n");
-        System.out.println("The quantity of these products are low, you should order some more.");
-    }*/
 }
