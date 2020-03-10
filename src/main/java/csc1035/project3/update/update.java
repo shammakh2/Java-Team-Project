798890698481c3dbd358e9075ddcc2b6b376eb38
Tom England (UG) <b9029625@newcastle.ac.uk>
2020-03-10 13:04:09 +0000

    Reworked update to remove duplicate code

--- src/main/java/csc1035/project3/update/update.java
+++ src/main/java/csc1035/project3/update/update.java
@@ -15,133 +15,48 @@ public class update {
         Scanner scanner = new Scanner(System.in);
         info = scanner.nextLine();
 
-        if (info.equalsIgnoreCase("name"))
-            try {
-                String id;
-                String name;
-                System.out.println("\n" + "Enter the ID of the object you want to change: ");
-                id = scanner.nextLine();
-                int v = Integer.parseInt(id);
-                System.out.println("\n" + "Enter the new name of the product: ");
-                name = scanner.nextLine();
-                session.beginTransaction();
-                Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
-                table_initializer.setName(name);
-                session.update(table_initializer);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                if (session != null) session.getTransaction().rollback();
-                System.out.println("Not a valid entry");
-            } finally {
-                session.close();
-            }
-        else if (info.equalsIgnoreCase("category"))
-            try {
-                String id;
-                String category;
-                System.out.println("\n" + "Enter the ID of the object you want to change: ");
-                id = scanner.nextLine();
-                int v = Integer.parseInt(id);
-                System.out.println("\n" + "Enter the new category of the product: ");
-                category = scanner.nextLine();
-                session.beginTransaction();
-                Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
-                table_initializer.setCategory(category);
-                session.update(table_initializer);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                if (session != null) session.getTransaction().rollback();
-                System.out.println("Not a valid entry");
-            } finally {
-                session.close();
-            }
-        else if (info.equalsIgnoreCase("perishable"))
-            try {
-                String id;
-                String perishable;
-                System.out.println("\n" + "Enter the ID of the object you want to change: ");
-                id = scanner.nextLine();
-                int v = Integer.parseInt(id);
-                System.out.println("\n" + "Enter if the product is perishable: true or false: ");
-                perishable = scanner.nextLine();
-                if (perishable.equalsIgnoreCase("true") || perishable.equalsIgnoreCase("false")){
-                boolean b = Boolean.parseBoolean(perishable);
-                session.beginTransaction();
-                Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
-                table_initializer.setPerishable(b);
-                session.update(table_initializer);
-                session.getTransaction().commit();}
-                else System.out.println("Not a valid entry.");
-            } catch (Exception e) {
-                if (session != null) session.getTransaction().rollback();
-                System.out.println("Not a valid entry");
-            } finally {
-                session.close();
-            }
-        else if (info.equalsIgnoreCase("cost"))
-            try {
-                String id;
-                String cost;
-                System.out.println("\n" + "Enter the ID of the object you want to change: ");
-                id = scanner.nextLine();
-                int v = Integer.parseInt(id);
-                System.out.println("\n" + "Enter the new name of the product: ");
-                cost = scanner.nextLine();
-                float f = Float.parseFloat(cost);
-                session.beginTransaction();
-                Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
-                table_initializer.setCost(f);
-                session.update(table_initializer);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                if (session != null) session.getTransaction().rollback();
-                System.out.println("Not a valid cost");
-            } finally {
-                session.close();
+        try {
+            String id;
+            String new_data;
+            System.out.println("\n" + "Enter the ID of the object you want to change: ");
+            id = scanner.nextLine();
+            int v = Integer.parseInt(id);
+            System.out.println("\n" + "Enter the new " + info + " of the product: ");
+            new_data = scanner.nextLine();
+            session.beginTransaction();
+            Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
+            switch (info.toLowerCase()) {
+                case "name":
+                    table_initializer.setName(new_data);
+                    break;
+                case "category":
+                    table_initializer.setCategory(new_data);
+                    break;
+                case "perishable":
+                    table_initializer.setPerishable(Boolean.parseBoolean(new_data));
+                    break;
+                case "cost":
+                    table_initializer.setCost(Float.parseFloat(new_data));
+                    break;
+                case "quantity":
+                    table_initializer.setCost(Integer.parseInt(new_data));
+                    break;
+                case "sell price":
+                    table_initializer.setSell_price(Float.parseFloat(new_data));
+                    break;
+                default:
+                    System.out.println("Not a valid option");
+                    break;
 
             }
-        else if (info.equalsIgnoreCase("quantity"))
-            try {
-                String id;
-                String quantity;
-                System.out.println("\n" + "Enter the ID of the object you want to change: ");
-                id = scanner.nextLine();
-                int v = Integer.parseInt(id);
-                System.out.println("\n" + "Enter the new quantity of the product: ");
-                quantity = scanner.nextLine();
-                int i = Integer.parseInt(quantity);
-                session.beginTransaction();
-                Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
-                table_initializer.setStock(i);
-                session.update(table_initializer);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                if (session != null) session.getTransaction().rollback();
-                System.out.println("Not a valid entry");
-            } finally {
-                session.close();
-            }
-        else if (info.equalsIgnoreCase("sell price"))
-            try {
-                String id;
-                String sell_price;
-                System.out.println("\n" + "Enter the ID of the object you want to change: ");
-                id = scanner.nextLine();
-                int v = Integer.parseInt(id);
-                System.out.println("\n" + "Enter the new sell price of the product: ");
-                sell_price = scanner.nextLine();
-                float f = Float.parseFloat(sell_price);
-                session.beginTransaction();
-                Table_Initializer table_initializer = (session.get(Table_Initializer.class, v));
-                table_initializer.setSell_price(f);
-                session.update(table_initializer);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                if (session != null) session.getTransaction().rollback();
-                System.out.println("Not a valid sell price");
-            } finally {
-                session.close();
-            }
-        else System.out.println("Not a valid entry please try again");
+
+            session.update(table_initializer);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session != null) session.getTransaction().rollback();
+            System.out.println("Not a valid entry");
+        } finally {
+            session.close();
+        }
     }
 }
