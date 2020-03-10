2ed1ac5506694475e8eaf4fe7cdb70f6004f5bd3
Nicholas Ingram <b9018614@newcastle.ac.uk>
2020-03-10 10:25:01 +0000

    Pushing in order to view Shams branch

--- src/main/java/csc1035/project3/PrintReceipt.java
+++ src/main/java/csc1035/project3/PrintReceipt.java
@@ -1,11 +1,31 @@
 package csc1035.project3;
 
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PrintReceipt {
-    public static void main(String[] args) {
-        AsciiTable();
+    public static void main(String[] args) throws IOException {
+        // Test Running
+
+        //NewAsciiTableReceipt();
+        //ReceiptToFile();
+        tempShoppingList();
     }
-    public static void AsciiTable() {
+
+    public static void tempShoppingList(){
+        // This simulates the data I obtain from the shopping basket/ transaction.
+        int fakeID = 244351324;
+        int fakeQuantity = 7;
+        float fakePrice = 3.40f;
+        ArrayList<ShoppingList> tempArray = new ArrayList<>();
+        ShoppingList tempList = new ShoppingList(fakeID, fakeQuantity, fakePrice);
+        tempArray.add(tempList);
+        NewAsciiTableReceipt(tempArray);
+    }
+
+    public static void NewAsciiTableReceipt(ArrayList<ShoppingList> asciiForReceipt) {
         // Possibly take a collection which will contain the Item ID, price, quantity.
 
         String leftAlignFormat = "| %-15s | £%6.2f | %-8d | £%7.2f |%n";
@@ -22,11 +42,28 @@ public class PrintReceipt {
         System.out.format("+-----------------+---------+----------+----------+%n");
 
         // Perform as loop for each product.
-        System.out.format(leftAlignFormat, test1, test2, test3, test4);
+        for (ShoppingList data : asciiForReceipt){
+            System.out.format(leftAlignFormat, data.getProductID(), data.getPrice(), data.getQuantity(), (data.getPrice() * data.getQuantity()));
+        }
+
         // for every item in array/collection produce a quantity * price and add to total.
         System.out.format("+-----------------+---------+----------+----------+%n");
         System.out.format("The final total paid: £%.2f%n", finalTotal);
 
 
+    }
+    public static void ReceiptToFile() throws IOException {
+        // Write the purchase receipt to a file rather than to console
+
+        FileWriter receiptPrint = new FileWriter("Receipts.txt", true);
+        receiptPrint.append(String.format("%n"));
+        receiptPrint.append("New Receipt:");
+        receiptPrint.append(String.format("%n"));
+        receiptPrint.append(String.format("+-----------------+---------+----------+----------+%n"));
+        receiptPrint.append(String.format("|     Product     |  Price  | Quantity |   Total  |%n"));
+        receiptPrint.append(String.format("+-----------------+---------+----------+----------+%n"));
+        //Append each product
+        receiptPrint.flush();
+        receiptPrint.close();
     }
 }
